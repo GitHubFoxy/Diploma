02c0d59c9d3b0a1f0e0b6b89e7d71e7d56a41a73
beruseruko <ambrosayos@mail.ru>
2024-02-11 17:49:14 +0700

    connected to docker mongoDB

--- backend/node/index.js
+++ backend/node/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = 3001;
 
+mongoose.connect("mongodb://localhost:27017/diploma");
+
+const UserSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const UserModel = mongoose.model("users", UserSchema);
+
 app.get("/", (req, res) => {
-  res.send("Hello from NodeJS");
+  UserModel.find({}).then(function (data) {
+    res.json(data);
+  });
 });
 
 app.listen(port, () => {
