905d463540d1cd7025b1dc314c2ac9d789bb90de
beruseruko <ambrosayos@mail.ru>
2024-02-20 15:41:29 +0700

    register ui ready

--- backend/php/index.php
+++ backend/php/index.php
@@ -2,7 +2,10 @@
 if($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = file_get_contents("php://input");
     $data = json_decode($data);
-    echo $data->login;
+    echo "data->type: ". $data->type;
+    if($data->type == "login") {
+        echo "login->" . $data->login . " password->" . $data->password;
+    }
 
 }
 ?>
